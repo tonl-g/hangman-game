0a854083543d8c6f68b048dcfbeea426a574b4eb
tonl-g <tlygonneville.dev@gmail.com>
2021-04-02 17:13:25 +0200

    Modif

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const readlineSync = require('readline-sync')
 const { readFileSync } = require('fs')
 const { randomInt } = require('crypto')
-/*
+
 // Start game
 if (readlineSync.keyInYNStrict(chalk.blue(`Do you want play Hangman's Game? `))) {
   // 'y' key was pressed.
@@ -13,7 +13,7 @@ if (readlineSync.keyInYNStrict(chalk.blue(`Do you want play Hangman's Game? `)))
   console.log('Bye')
   process.exit(0)
   } 
-*/
+
 // Read dictionnary
 const readDic = readFileSync('./dictionnary.txt', 'utf-8')
 // console.log(readDic)
@@ -24,7 +24,7 @@ const wordsDic = readDic.toLowerCase().split('\n')
 
 // Random array
 const randWord = randomInt(0, wordsDic.length)
-console.log(wordsDic[randWord]) 
+console.log(wordsDic[randWord]) // Test word
 
 // Boucle
 let isRunning = true
@@ -32,10 +32,11 @@ let isRunning = true
 while (isRunning) {
   const findWord = readlineSync.question(chalk.blue('Guess the word: '))
   const matchLetters = ((wordsDic[randWord]).match(findWord))
+  // const counter = 0 / A faire
 
   if (findWord != (wordsDic[randWord])) {
-    console.log(chalk.red(`It's not the good word`))
-    console.log(chalk.green(`Good letters: ${matchLetters}`))
+    console.log(chalk.red(`It's not the good word!`))
+    console.log(chalk.green(`Good: ${matchLetters}`))
     if (readlineSync.keyInYNStrict(`Do you want to continue? `)) {
       // 'y' key was pressed.
       console.log('Restart')
@@ -46,9 +47,11 @@ while (isRunning) {
       process.exit(0)
       } 
   } else { (findWord === (wordsDic[randWord])) 
-    console.log(chalk.green(`Congratulation! You find the good word '${(wordsDic[randWord])}'`))
+    console.log(chalk.green(`Congratulation! You find the good word '${(wordsDic[randWord])}'!!!`))
     // Exit the loop
     isRunning = false
   }
 }
+
+
 
