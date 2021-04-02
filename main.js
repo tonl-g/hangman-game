09be0745c6095f2c84af26ae4a15dfc97455d94f
tonl-g <tlygonneville.dev@gmail.com>
2021-04-02 15:36:23 +0200

    Modif

--- main.js
+++ main.js
@@ -3,35 +3,47 @@ const readlineSync = require('readline-sync')
 const { readFileSync } = require('fs')
 const { randomInt } = require('crypto')
 
-/* // Start game
+// Start game
 if (readlineSync.keyInYNStrict(`Do you want play Hangman's Game? `)) {
   // 'Y' key was pressed.
-  console.log('Continue...')
+  console.log('Start')
   // continuer le programme
   } else {
   // 'N' key was pressded.
-  console.log('Goodbye!')
+  console.log('Bye')
   process.exit(0)
-  } */
+  } 
 
-//
+// Read dictionnary
 const readDic = readFileSync('./dictionnary.txt', 'utf-8')
 // console.log(readDic)
 
+// Create array 
 const wordsDic = readDic.toLowerCase().split('\n')
 // console.log(words)
 
+// Random array
 const randWord = randomInt(0, wordsDic.length)
 console.log(wordsDic[randWord]) 
 
+// Boucle
 let isRunning = true
 while (isRunning) {
-  const findWord = readlineSync.question('Guess a word: ')
+  const findWord = readlineSync.question('Guess the word: ')
   if (findWord != (wordsDic[randWord])) {
-    console.log(`It's not a good word`)
+    console.log(`It's not the good word`)
+    if (readlineSync.keyInYNStrict(`Do you want to continue? `)) {
+      // 'Y' key was pressed.
+      console.log('Restart')
+      // continuer le programme
+      } else {
+      // 'N' key was pressded.
+      console.log('Bye')
+      process.exit(0)
+      } 
   } else { (findWord === (wordsDic[randWord])) 
-    console.log(`You find the good word ${(wordsDic[randWord])}`)
-    // exit the loop
+    console.log(`Congratulation! You find the good word '${(wordsDic[randWord])}'`)
+    // Exit the loop
     isRunning = false
   }
 }
